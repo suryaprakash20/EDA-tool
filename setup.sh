417331d021ee618f3af6a8c4ee2004c9ce73f41a
surya prakash <63799262+suryaprakash20@users.noreply.github.com>
2021-03-06 10:59:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ mkdir -p ~/.streamlit/
 
 echo "[server]
 headless = true
-port = $PORT
+port = run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
 enableCORS = false
 " > ~/.streamlit/config.toml
